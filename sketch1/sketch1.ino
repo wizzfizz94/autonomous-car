2c6dea6ea8fe1bb516c35525488cbb9b83205edc
wizzfizz94 <21149116@student.uwa.edu.au>
2017-10-06 09:15:22 +0800

    sketch stub

---
+++ sketch1/sketch1.ino
@@ -0,0 +1,31 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sv = getSensorValue();
+  int r_mot = calcPID();
+  setMotorOutput()
+
+  if(//){
+    //turn right
+  else if(//){
+    //turn left
+  } else {
+    //go straight
+  }
+}
+
+int getSensorValue(){
+  return 0;
+}
+
+int calcPID(){
+  return 0;  
+}
+
+int setMotorOutput(){
+  return 0;
+}
