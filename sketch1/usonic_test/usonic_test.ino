76a9ec0896b73aa3bf02512e7e2d9c70e751eee6
wizzfizz94 <21149116@student.uwa.edu.au>
2017-10-12 13:09:42 +0800

    wall following done

--- sketch1/usonic_test/usonic_test.ino
+++ sketch1/usonic_test/usonic_test.ino
@@ -1,18 +1,5 @@
-// Ultrasonic HC-SR04 unit interface
-// Uses serial port at 115200 baud for communication
-// use trig pin for output, echo pin for input
-// pulse out (10us) on trig initiates a cycle
-// pulse width returned on echo is proportional to distance
-// specs say 38ms = no return (beyond limit), but 90ms and more have been seen
-// set utimeout when calling usonic (routine will take longer for longer returns)
-// higher timeout measures further, but can take longer if no echo
-// if return >= utimeout, no valid pulse received
-// if return < ~100 unit is faulty/disconnected (routine is timing out waiting for start of return)
-// if return == 0 then unit is still sending return from last ping (or is faulty)
-// maximum nominal range is 5m => utimeout = 29000 us
-// call usonicsetup() during setup
-// call usonic(timeout) to get return time in microseconds
-// divide result of usonic by 58 to get range in cm
+#include <stdio.h>
+#include <stdlib.h>
 
 //define pins here
 #define TRIG_LEFT 2
@@ -31,22 +18,19 @@
 #define DRIVE_PIN 11
 
 
-#define MIN_DIST    150 // mm
-#define MAX_DIST    160 // mm
+#define MIN_DIST    15 // cm
+#define MAX_DIST    16 // cm
 
 #define CORRECT_ANGLE   5 // degrees
 
-#define STEP_DIST   5   // mm
+#define STEP_DIST   1   // cm
 #define STEP_DIST_SPEED   250 // mm
 #define STEP_TURN   -90   // degrees
 #define STEP_TURN_SPEED   45    // degrees
 
-#define FRONT_PSD   2 // Front distance sensor
-#define LEFT_PSD    1 // Left distance sensor
-
 void setup() {
   
-  Serial.begin(9600);
+ Serial.begin(9600);
  
  // initialize digital pin LED_BUILTIN as an output.
  pinMode(LED_BUILTIN, OUTPUT);
@@ -56,51 +40,49 @@ void setup() {
  pinMode(DRIVE_PIN, OUTPUT);
  
  usonicsetup();            //set up ultrasonic sensor
-
- analogWrite(DRIVE_PIN, 5); //drive at slower speed
  
 }
 
 void loop() {
   
- int d_left, d_right;
+  int d_left, d_right, d_front;
+  //distance in cm, time out at 11600us or 2m maximum range
+  d_left = usonic(11600, 2) / 58;
+  d_right = usonic(11600, 4) / 58;
+  d_front = usonic(11600, 4) / 58;
 
- //distance in cm, time out at 11600us or 2m maximum range
- d_left = usonic(11600, 2) / 58;
- d_right = usonic(11600, 4) / 58;
+  if(d_left < 10 && d_right < 10 && d_front < 10) exit(0);
 
- Serial.print(d_left);
- Serial.print("\n");
- 
- Serial.print(d_right);
- Serial.print("\n");
- 
- if (d_left < 10) {
-  digitalWrite(RIGHT_TURN_PIN, HIGH);
- } 
- if (d_right < 10) {
-  digitalWrite(LEFT_TURN_PIN, HIGH);
- }
- delay(100);              
- //TODO findout if we actually need this when not using serial port
+  char str[100];
+  sprintf(str, "d_left = %d, d_right = %d, d_front = %d\n", d_left, d_right, d_front);
+  Serial.print(str);
+  
+  if (d_left < 200) {
 
-  int forward_dist = PSDGet(FRONT_PSD);
-  int left_dist = PSDGet(LEFT_PSD);
-
-  while (current_turn < NUM_TURNS) {
-    VWStraight(STEP_DIST, STEP_DIST_SPEED);
-    if (forward_dist < MIN_DIST) {
-      VWTurn(STEP_TURN, STEP_TURN_SPEED);
-      VWWait();
-      current_turn++;
-    } else if (left_dist < MIN_DIST) {
-      VWTurn(-1*CORRECT_ANGLE, STEP_TURN_SPEED);
-    } else if (left_dist > MAX_DIST) {
-      VWTurn(CORRECT_ANGLE, STEP_TURN_SPEED);
+    //distance in cm, time out at 11600us or 2m maximum range
+    d_left = usonic(11600, 2) / 58;
+    d_right = usonic(11600, 4) / 58;
+    d_front = usonic(11600, 4) / 58;
+    
+    analogWrite(DRIVE_PIN, 5);
+    if (d_left < MIN_DIST) {
+      digitalWrite(RIGHT_TURN_PIN, HIGH);
+    } else if (d_left > MAX_DIST) {
+      digitalWrite(LEFT_TURN_PIN, HIGH);
+    } else {
+      digitalWrite(RIGHT_TURN_PIN, LOW);
+      digitalWrite(LEFT_TURN_PIN, LOW);
     }
-    forward_dist = PSDGet(FRONT_PSD);
-    left_dist = PSDGet(LEFT_PSD);
+    
   }
+  
+  else {
+
+    d_front = usonic(11600, 4) / 58;
+    digitalWrite(LEFT_TURN_PIN, HIGH);
+    
+  }
+  
 }
 
 void usonicsetup(void) {
@@ -141,4 +123,5 @@ long usonic(long utimeout, int trig_pin) {
  b = micros() - utimer;
  if(b == 0) b = utimeout;
  return b;
+ 
 }
